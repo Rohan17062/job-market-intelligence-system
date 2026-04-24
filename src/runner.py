1b9d53a94c8ec091d01dacbbaef086da726d7b1b
Rohan17062 <rp17062005@gmail.com>
2026-04-25 00:02:30 +0530

    Adding runner.py file

---
+++ src/runner.py
@@ -0,0 +1,39 @@
+import sqlite3
+import pipeline
+import analysis
+import pandas as pd
+
+print("\n====RUNNING SYSTEM ====")
+
+conn=sqlite3.connect("jobss.db")
+print("\n===Running Pipeline ===")
+pipeline.run_pipeline(conn)
+
+print("\n--Checking Alerts--")
+from alertt import AlertSystem
+ale=AlertSystem(conn)
+ale.check_job_drop_spike_alert()
+query="""
+select * from alerts
+"""
+df=pd.read_sql(query,conn)
+df.to_csv("alerts_table.csv",index=False)
+print("alerts csv updated succesfully")
+
+
+print("\n--Running Analysis--")
+analysis.run_all_analysis(conn)
+
+print("\n--Exporting data for power bi--")
+
+
+query="""
+select * from jobs
+"""
+df=pd.read_sql(query,conn)
+df.to_csv("jobs_data.csv",index=False)
+print("csv updated succesfully")
+
+conn.close()
+print("System running complete")
+
