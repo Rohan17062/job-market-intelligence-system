810cc49387b04216ebe4da60e9010e21001f5b90
Rohan17062 <rp17062005@gmail.com>
2026-04-25 12:42:44 +0530

    updating data_release code

--- src/data_release.py
+++ src/data_release.py
@@ -2,22 +2,31 @@ import pandas as pd
 import os
 import random
 
-master=pd.read_csv("ai_job_market.csv")
 
-if os.path.exists("jobs_incoming.csv") and os.path.getsize("jobs_incoming.csv")>0:
-    incoming=pd.read_csv("jobs_incoming.csv")
+master = pd.read_csv("data/ai_job_market.csv")
+
+# incoming file path
+incoming_path = "data/jobs_incoming.csv"
+
+# check if file exists
+if os.path.exists(incoming_path) and os.path.getsize(incoming_path) > 0:
+    incoming = pd.read_csv(incoming_path)
 else:
-    incoming=pd.DataFrame(columns=master.columns)
+    incoming = pd.DataFrame(columns=master.columns)
 
-current_size=len(incoming)
-batch_size=random.choice([10,35,80,134,55,180])
+current_size = len(incoming)
 
-end_index=min(current_size+batch_size,len(master))
-next_batch=master.iloc[current_size:end_index]
+batch_size = random.choice([10, 35, 80, 134, 55, 180])
+
+end_index = min(current_size + batch_size, len(master))
+next_batch = master.iloc[current_size:end_index]
 
 if next_batch.empty:
     print("no more data left")
 else:
-    updated=pd.concat([incoming,next_batch],ignore_index=True)
-    updated.to_csv("jobs_incoming.csv",index=False)
-    print(f"added {len(next_batch)} new rows.now total rows are {len(updated)}")
+    updated = pd.concat([incoming, next_batch], ignore_index=True)
+
+    # save back to data folder
+    updated.to_csv(incoming_path, index=False)
+
+    print(f"added {len(next_batch)} new rows. now total rows are {len(updated)}")
